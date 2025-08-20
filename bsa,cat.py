85eb094f70db1f9fabbc4b3bef954c8d960170dc
andresf006 <pipemelo08@gmail.com>
2025-08-19 22:10:31 -0500

    bsa,cat.py

--- bsa,cat.py
+++ bsa,cat.py
@@ -9,17 +9,17 @@ def bsa(height_cm, weight_kg):
 h = float(input("Altura (cm): "))
 w = float(input("Peso (kg): "))
 area = bsa(h, w)
-print(f"BSA estimada: {area:.2f} m²")
+print(f"BSA estimado: {area:.2f} m²")
     
 if area <= 0.25:
-        print (f"Se encuentra en el rango BSA de Recién nacido")
+        print (f"Usted se encuentra en el rango BSA de: Recién nacido")
 elif area <= 0.5:
-        print (f"Se encuentra en el rango BSA de Lactante")
+        print (f"Usted se encuentra en el rango BSA de: Lactante")
 elif area <= 1.14:
-        print (f"Se encuentra en el rango BSA de niño")
+        print (f"Usted se encuentra en el rango BSA de: Niño")
 elif area <= 1.6:
-        print (f"Se encuentra en el rango BSA de Mujer adulta")
+        print (f"Usted se encuentra en el rango BSA de: Mujer adulta")
 elif area <= 1.9:
-        print (f"Se encuentra en el rango BSA de Hombre adulto")
+        print (f"Usted se encuentra en el rango BSA de: Hombre adulto")
 else:
-        print(f"Se encuentra en el rango BSA de adultos muy altos/pesados")
+        print(f"Usted se encuentra en el rango BSA de: Adultos muy altos/pesados")
